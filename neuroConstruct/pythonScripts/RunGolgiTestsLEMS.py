2ee0b2780e790383aa8348c1a728f0e508d6f5cc
Padraig Gleeson <p.gleeson@gmail.com>
2013-09-02 13:51:13 +0100

    Adding test for golgi single cell

---
+++ neuroConstruct/pythonScripts/RunGolgiTestsLEMS.py
@@ -0,0 +1,97 @@
+#
+#
+#   File to test behaviour of the Golgi Cell.
+#
+#   To execute this type of file, type '..\..\..\nC.bat -python XXX.py' (Windows)
+#   or '../../../nC.sh -python XXX.py' (Linux/Mac). Note: you may have to update the
+#   NC_HOME and NC_MAX_MEMORY variables in nC.bat/nC.sh
+#
+#   Author: Padraig Gleeson
+#
+#   This file has been developed as part of the neuroConstruct project
+#   This work has been funded by the Medical Research Council and the
+#   Wellcome Trust
+#
+#
+
+import sys
+import os
+
+try:
+	from java.io import File
+except ImportError:
+	print "Note: this file should be run using ..\\..\\..\\nC.bat -python XXX.py' or '../../../nC.sh -python XXX.py'"
+	print "See http://www.neuroconstruct.org/docs/python.html for more details"
+	quit()
+
+sys.path.append(os.environ["NC_HOME"]+"/pythonNeuroML/nCUtils")
+
+import ncutils as nc
+
+projFile = File(os.getcwd(), "../GranCellLayer.ncx")
+
+
+##############  Main settings  ##################
+
+simConfigs = []
+
+#simConfigs.append("Default Simulation Configuration")
+simConfigs.append("Single Golgi Cell")
+
+simDt =                 0.001
+simDtOverride =         {"LEMS":0.00025}
+
+simulators =            ["NEURON", "GENESIS_PHYS", "LEMS"] # Note: nernst object isn't implemented in MOOSE yet
+
+varTimestepNeuron =     True
+varTimestepTolerance =  0.00001
+
+plotSims =              True
+plotVoltageOnly =       True
+runInBackground =       True
+analyseSims =           True
+verbose =               True
+
+#############################################
+
+
+def testAll(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    print "Loading project from "+ projFile.getCanonicalPath()
+
+
+    simManager = nc.SimulationManager(projFile,
+                                      verbose = verbose)
+
+    simManager.runMultipleSims(simConfigs =           simConfigs,
+                               simDt =                simDt,
+                               simDtOverride =        simDtOverride,
+                               simulators =           simulators,
+                               runInBackground =      runInBackground,
+                               varTimestepNeuron =    varTimestepNeuron,
+                               varTimestepTolerance = varTimestepTolerance)
+
+    simManager.reloadSims(plotVoltageOnly =   plotVoltageOnly,
+                          plotSims =          plotSims,
+                          analyseSims =       analyseSims)
+
+    # These were discovered using analyseSims = True above.
+    # They need to hold for all simulators
+    spikeTimesToCheck = {'SingleGolgi_0': [12.2, 33.5, 93.0, 197.4, 310.1, 424.8,
+                                           508.0, 529.3, 564.5, 613.8, 668.3, 724.1, 780.2,
+                                           836.6, 893.0, 949.5, 1157.6, 1277.6, 1394.4]}
+
+    spikeTimeAccuracy = 4  # Note run time of 1500 ms...
+
+    report = simManager.checkSims(spikeTimesToCheck = spikeTimesToCheck,
+                                  spikeTimeAccuracy = spikeTimeAccuracy)
+
+    print report
+
+    return report
+
+
+if __name__ == "__main__":
+    testAll()
